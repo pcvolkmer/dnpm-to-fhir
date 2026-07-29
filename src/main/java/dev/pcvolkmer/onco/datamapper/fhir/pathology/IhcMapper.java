f86ae05660741a6055fd1beac319fd34d01bb3b0
Alexander Schulz <schulz.alexander@uk-erlangen.de>
2026-07-29 18:09:12 +0200

    fix: makes example-data more realistic and updates IhcMapper to build the correct sub-profiles

--- src/main/java/dev/pcvolkmer/onco/datamapper/fhir/pathology/IhcMapper.java
+++ src/main/java/dev/pcvolkmer/onco/datamapper/fhir/pathology/IhcMapper.java
@@ -23,6 +23,7 @@ import dev.pcvolkmer.mv64e.model.IhcReport;
 import dev.pcvolkmer.mv64e.model.ProteinExpression;
 import dev.pcvolkmer.onco.datamapper.fhir.ManyMapper;
 import dev.pcvolkmer.onco.datamapper.fhir.ObservationMapper;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -30,11 +31,6 @@ import org.hl7.fhir.r4.model.*;
 
 public class IhcMapper extends ObservationMapper<ProteinExpression>
     implements ManyMapper<IhcReport, Observation> {
-
-  // TODO: Unterscheidung PDL1 / HER2 / generisch
-  // FIXME: vermutlich falsche Testdaten?
-  //  TPS/CPS/ICS/TC-Scores auch bei nicht PDL1 Genen?
-  //  1+/2+/3+ Codes außerhalb von HER2?
 
   @Override
   protected String getPatientId(ProteinExpression item) {
@@ -49,14 +45,11 @@ public class IhcMapper extends ObservationMapper<ProteinExpression>
   @Override
   public Observation map(ProteinExpression sourceItem) {
     var result = new Observation();
+    var profile = resolveProfile(sourceItem);
 
     result.addIdentifier().setSystem(this.getSystem()).setValue(this.getId(sourceItem));
 
-    result.setMeta(
-        new Meta()
-            .setSource(this.fhirMetaSource)
-            .addProfile(
-                "https://www.medizininformatik-initiative.de/fhir/ext/modul-mtb/StructureDefinition/mii-pr-mtb-immunohistochemistry"));
+    result.setMeta(new Meta().setSource(this.fhirMetaSource).addProfile(profile.getCanonicalUrl()));
 
     result.setStatus(Observation.ObservationStatus.FINAL);
 
@@ -81,13 +74,38 @@ public class IhcMapper extends ObservationMapper<ProteinExpression>
                     .setCode("1234806008")
                     .setDisplay("Observation using immunohistochemistry (observable entity)")));
 
-    result.setValue(
-        new CodeableConcept()
-            .addCoding(
-                new Coding()
-                    .setSystem(sourceItem.getValue().getSystem())
-                    .setCode(sourceItem.getValue().getCode().getValue())
-                    .setDisplay(sourceItem.getValue().getDisplay())));
+    if (profile == IhcProfile.HER2) {
+      result.setCode(
+          new CodeableConcept()
+              .addCoding(
+                  new Coding()
+                      .setSystem("http://loinc.org")
+                      .setCode("18474-7")
+                      .setDisplay("HER2 Ag [Presence] in Tissue by Immune stain")));
+    }
+
+    // value or dataAbsentReason if unknown
+    // TODO: oder ist "unknown" eher ein gültiges value?
+    // Datenmodell sagt: "Untersucht, kein Ergebnis"
+    if ("unknown".equals(sourceItem.getValue().getCode().getValue())) {
+      result.setDataAbsentReason(
+          new CodeableConcept()
+              .addCoding(
+                  new Coding()
+                      .setSystem("http://terminology.hl7.org/CodeSystem/data-absent-reason")
+                      .setCode("unknown")
+                      .setDisplay("Unknown")));
+    } else if (profile == IhcProfile.HER2) {
+      result.setValue(mapHer2ResultValue(sourceItem.getValue().getCode().getValue()));
+    } else {
+      result.setValue(
+          new CodeableConcept()
+              .addCoding(
+                  new Coding()
+                      .setSystem(sourceItem.getValue().getSystem())
+                      .setCode(sourceItem.getValue().getCode().getValue())
+                      .setDisplay(sourceItem.getValue().getDisplay())));
+    }
 
     // Gene
     result.addComponent(
@@ -107,9 +125,157 @@ public class IhcMapper extends ObservationMapper<ProteinExpression>
                             .setCode(sourceItem.getProtein().getCode())
                             .setDisplay(sourceItem.getProtein().getDisplay()))));
 
+    if (profile == IhcProfile.PDL1) {
+      addPdl1ScoreComponents(result, sourceItem);
+    }
+
     result.setSubject(this.getPatientReference(sourceItem));
 
     return result;
+  }
+
+  private IhcProfile resolveProfile(ProteinExpression sourceItem) {
+    if (sourceItem.getProtein() == null) {
+      throw new IllegalArgumentException("IHC protein must not be null");
+    }
+
+    var proteinCode = sourceItem.getProtein().getCode();
+
+    if ("HGNC:3430".equals(proteinCode)) {
+      return IhcProfile.HER2;
+    }
+
+    if ("HGNC:17635".equals(proteinCode)) {
+      return IhcProfile.PDL1;
+    }
+
+    return IhcProfile.GENERIC;
+  }
+
+  // using LL4396-9
+  private CodeableConcept mapHer2ResultValue(String resultCode) {
+    String loincCode;
+    String display;
+
+    switch (resultCode) {
+      case "0":
+        loincCode = "LA6111-4";
+        display = "0";
+        break;
+      case "1+":
+        loincCode = "LA11841-6";
+        display = "1+";
+        break;
+      case "2+":
+        loincCode = "LA11842-4";
+        display = "2+";
+        break;
+      case "3+":
+        loincCode = "LA11843-2";
+        display = "3+";
+        break;
+      case "indeterminate":
+        loincCode = "LA11884-6";
+        display = "Indeterminate";
+        break;
+      default:
+        throw new IllegalArgumentException(
+            String.format("Result code %s not suported for HER2 IHC", resultCode));
+    }
+
+    return new CodeableConcept()
+        .addCoding(
+            new Coding().setSystem("http://loinc.org").setCode(loincCode).setDisplay(display));
+  }
+
+  private void addPdl1ScoreComponents(Observation target, ProteinExpression sourceItem) {
+
+    if (sourceItem.getTpsScore() != null) {
+      target.addComponent(
+          new Observation.ObservationComponentComponent()
+              .setCode(
+                  new CodeableConcept()
+                      .addCoding(
+                          new Coding()
+                              .setSystem(
+                                  "https://nih-ncpi.github.io/ncpi-fhir-ig/CodeSystem-ncit.html")
+                              .setCode("C184941")
+                              .setDisplay("PD-L1 Tumor Proportion Score")))
+              .setValue(percentageQuantity(sourceItem.getTpsScore())));
+    }
+
+    if (sourceItem.getCpsScore() != null) {
+      target.addComponent(
+          new Observation.ObservationComponentComponent()
+              .setCode(
+                  new CodeableConcept()
+                      .addCoding(
+                          new Coding()
+                              .setSystem(
+                                  "https://nih-ncpi.github.io/ncpi-fhir-ig/CodeSystem-ncit.html")
+                              .setCode("C176582")
+                              .setDisplay("PD-L1 Combined Positive Score")))
+              .setValue(new Quantity().setValue(BigDecimal.valueOf(sourceItem.getCpsScore()))));
+    }
+
+    if (sourceItem.getIcScore() != null) {
+      target.addComponent(
+          new Observation.ObservationComponentComponent()
+              .setCode(
+                  new CodeableConcept()
+                      .addCoding(
+                          new Coding()
+                              .setSystem(
+                                  "https://nih-ncpi.github.io/ncpi-fhir-ig/CodeSystem-ncit.html")
+                              .setCode("C199175")
+                              .setDisplay("PD-L1 Immune Cell Score")))
+              .setValue(thresholdPercentageQuantity(sourceItem.getIcScore().getCode().getValue())));
+    }
+
+    if (sourceItem.getTcScore() != null) {
+      target.addComponent(
+          new Observation.ObservationComponentComponent()
+              .setCode(
+                  new CodeableConcept()
+                      .addCoding(
+                          new Coding()
+                              .setSystem("http://loinc.org")
+                              .setCode("83053-9")
+                              .setDisplay(
+                                  "Cells.programmed cell death ligand 1/Viable tumor cells in Tissue by Immune stain")))
+              .setValue(thresholdPercentageQuantity(sourceItem.getTcScore().getCode().getValue())));
+    }
+  }
+
+  private Quantity percentageQuantity(Integer value) {
+    return new Quantity()
+        .setValue(BigDecimal.valueOf(value))
+        .setUnit("%")
+        .setSystem("http://unitsofmeasure.org")
+        .setCode("%");
+  }
+
+  // For Ics-Score only codes 0-3 should be set. Tc-Score-codes go from 0 to 6
+  private Quantity thresholdPercentageQuantity(String sourceCode) {
+    switch (sourceCode) {
+      case "0":
+        return percentageQuantity(1).setComparator(Quantity.QuantityComparator.LESS_THAN);
+      case "1":
+        return percentageQuantity(1).setComparator(Quantity.QuantityComparator.GREATER_OR_EQUAL);
+      case "2":
+        return percentageQuantity(5).setComparator(Quantity.QuantityComparator.GREATER_OR_EQUAL);
+      case "3":
+        return percentageQuantity(10).setComparator(Quantity.QuantityComparator.GREATER_OR_EQUAL);
+      case "4":
+        return percentageQuantity(25).setComparator(Quantity.QuantityComparator.GREATER_OR_EQUAL);
+      case "5":
+        return percentageQuantity(50).setComparator(Quantity.QuantityComparator.GREATER_OR_EQUAL);
+      case "6":
+        return percentageQuantity(75).setComparator(Quantity.QuantityComparator.GREATER_OR_EQUAL);
+      default:
+        throw new IllegalArgumentException(
+            String.format("Unsupported score code %s for PD-L1 IC/TC-Scores", sourceCode));
+    }
   }
 
   @Override
@@ -136,5 +302,24 @@ public class IhcMapper extends ObservationMapper<ProteinExpression>
     return sourceItem.getResults().getProteinExpression().stream()
         .filter(Objects::nonNull)
         .collect(Collectors.toList());
+  }
+
+  private enum IhcProfile {
+    GENERIC(
+        "https://www.medizininformatik-initiative.de/fhir/ext/modul-mtb/StructureDefinition/mii-pr-mtb-immunohistochemistry"),
+    HER2(
+        "https://www.medizininformatik-initiative.de/fhir/ext/modul-mtb/StructureDefinition/mii-pr-mtb-immunohistochemistry-her2"),
+    PDL1(
+        "https://www.medizininformatik-initiative.de/fhir/ext/modul-mtb/StructureDefinition/mii-pr-mtb-immunohistochemistry-pdl1");
+
+    private final String canonicalUrl;
+
+    IhcProfile(String canonicalUrl) {
+      this.canonicalUrl = canonicalUrl;
+    }
+
+    private String getCanonicalUrl() {
+      return canonicalUrl;
+    }
   }
 }
