804b81878d5f3a47022a42804e2193f036ad578a
Alexander Schulz <schulz.alexander@uk-erlangen.de>
2026-08-03 13:52:41 +0200

    Feat: Add IhcMapper and MolekularPathologieBefundMapper (#5)

--- src/main/java/dev/pcvolkmer/onco/datamapper/fhir/DnpmToFhirMapper.java
+++ src/main/java/dev/pcvolkmer/onco/datamapper/fhir/DnpmToFhirMapper.java
@@ -31,6 +31,8 @@ import dev.pcvolkmer.onco.datamapper.fhir.careplan.TherapieempfehlungMapper;
 import dev.pcvolkmer.onco.datamapper.fhir.careplan.TherapieplanMapper;
 import dev.pcvolkmer.onco.datamapper.fhir.diagnosis.*;
 import dev.pcvolkmer.onco.datamapper.fhir.ngs.*;
+import dev.pcvolkmer.onco.datamapper.fhir.pathology.IhcMapper;
+import dev.pcvolkmer.onco.datamapper.fhir.pathology.MolekularPathologieBefundMapper;
 import dev.pcvolkmer.onco.datamapper.fhir.tnm.TnmMMapper;
 import dev.pcvolkmer.onco.datamapper.fhir.tnm.TnmNMapper;
 import dev.pcvolkmer.onco.datamapper.fhir.tnm.TnmTMapper;
@@ -239,6 +241,18 @@ public abstract class DnpmToFhirMapper<S, D extends Resource> implements Mapper<
                   "MSI interpretation with MMR is not supported and ignored in msiFindings[{}]",
                   item.getId());
             }
+          });
+    }
+
+    final var ihcReports = patientRecord.getIhcReports();
+    if (ihcReports != null) {
+      final var ihcMapper = new IhcMapper();
+      final var molekularPathologieBefundMapper = new MolekularPathologieBefundMapper(ihcMapper);
+
+      ihcReports.forEach(
+          item -> {
+            ihcMapper.addManyToBundle(bundle, item);
+            molekularPathologieBefundMapper.addToBundle(bundle, item);
           });
     }
 
---
+++ src/main/java/dev/pcvolkmer/onco/datamapper/fhir/pathology/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package dev.pcvolkmer.onco.datamapper.fhir.pathology;
+
+import org.jspecify.annotations.NullMarked;
